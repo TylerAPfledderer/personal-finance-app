586d772e6fae9c7f9db6130fabc8f4e5ce7dff16
tylerapfledderer <tyler.pfledderer@gmail.com>
2024-09-15 23:04:23 -0400

    fix: expose public folder to historie

--- histoire.config.ts
+++ histoire.config.ts
@@ -6,4 +6,7 @@ export default defineConfig({
   plugins: [HstVue(), HstNuxt()],
   setupFile: "/utils/histoire-setup.ts",
   storyMatch: ["**/*.story.vue"],
+  vite: {
+    publicDir: "./public",
+  },
 });
