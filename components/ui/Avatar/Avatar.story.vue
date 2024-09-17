76ae9d07525bc89e2a6fdbde60522b8cf445d598
tylerapfledderer <tyler.pfledderer@gmail.com>
2024-09-17 09:23:57 -0400

    feat: build avatar

---
+++ components/ui/Avatar/Avatar.story.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import Avatar from "./index.vue";
+
+const avatarImgList = [
+  "aqua-flow-utilities.jpg",
+  "buzz-marketing-group.jpg",
+  "bytewise.jpg",
+  "daniel-carter.jpg",
+  "ecofuel-energy.jpg",
+  "elevate-education.jpg",
+  "ella-phillips.jpg",
+  "emma-richardson.jpg",
+  "ethan-clark.jpg",
+  "flavor-fiesta.jpg",
+  "green-plate-eatery.jpg",
+  "harper-edwards.jpg",
+  "james-thompson.jpg",
+  "liam-hughes.jpg",
+  "lily-ramirez.jpg",
+  "mason-martinez.jpg",
+  "nimbus-data-storage.jpg",
+  "pixel-playground.jpg",
+  "rina-sato.jpg",
+  "savory-bites-bistro.jpg",
+  "sebastian-cook.jpg",
+  "serenity-spa-and-wellness.jpg",
+  "sofia-peterson.jpg",
+  "spark-electric-solutions.jpg",
+  "sun-park.jpg",
+  "swift-ride-share.jpg",
+  "technova-innovations.jpg",
+  "urban-services-hub.jpg",
+  "william-harris.jpg",
+  "yuna-kim.jpg",
+];
+</script>
+<template>
+  <Story title="ui / Avatar">
+    <Variant title="md Size">
+      <div class="grid grid-cols-4 gap-8">
+        <Avatar
+          v-for="img in avatarImgList"
+          :key="img"
+          :src="`/images/avatars/${img}`"
+        />
+      </div>
+    </Variant>
+    <Variant title="sm Size">
+      <div class="grid grid-cols-4 gap-8">
+        <Avatar
+          v-for="img in avatarImgList"
+          :key="img"
+          :src="`/images/avatars/${img}`"
+          size="sm"
+        />
+      </div>
+    </Variant>
+  </Story>
+</template>
---
+++ components/ui/Avatar/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { Avatar, type AvatarImageProps } from "@ark-ui/vue";
+import { tv } from "tailwind-variants";
+import { mergeProps } from "vue";
+
+defineOptions({
+  inheritAttrs: false,
+});
+
+const avatarStyles = tv({
+  base: "rounded-full",
+  variants: {
+    size: {
+      md: "size-10",
+      sm: "size-8",
+    },
+  },
+  defaultVariants: {
+    size: "md",
+  },
+});
+
+const { size, ...props } = defineProps<
+  AvatarImageProps & { size?: "md" | "sm" }
+>();
+</script>
+<template>
+  <Avatar.Root>
+    <Avatar.Image
+      v-bind="mergeProps(props, $attrs)"
+      :class="avatarStyles({ size })"
+    />
+  </Avatar.Root>
+</template>
