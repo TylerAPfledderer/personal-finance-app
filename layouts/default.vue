fb43fe0626523bc0f8b06a7db5792bc1cb2d9d6a
tylerapfledderer <tyler.pfledderer@gmail.com>
2025-01-04 22:11:07 -0500

    refactor(layouts/default): update positioning on larger screens

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,11 +2,13 @@
 const defaultLayoutAttrs = useAttrs();
 </script>
 <template>
-  <Sidebar />
-  <main class="grid gap-8 px-4 py-6">
-    <h1 class="text-preset-1">
-      {{ defaultLayoutAttrs.defaultLayoutLabel }}
-    </h1>
-    <slot></slot>
-  </main>
+  <div class="flex">
+    <Sidebar />
+    <main class="grid w-full gap-8 px-4 py-6 md:px-10 md:py-8">
+      <h1 class="text-preset-1">
+        {{ defaultLayoutAttrs.defaultLayoutLabel }}
+      </h1>
+      <slot></slot>
+    </main>
+  </div>
 </template>
