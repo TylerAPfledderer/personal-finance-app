8a5b76e85702af9cf1dd139c6e36204936ca3c48
tylerapfledderer <tyler.pfledderer@gmail.com>
2025-01-04 22:18:44 -0500

    feat: add balances to overview page

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,9 @@ const defaultLayoutAttrs = useAttrs();
 <template>
   <div class="flex">
     <Sidebar />
-    <main class="grid w-full gap-8 px-4 py-6 md:px-10 md:py-8">
+    <main
+      class="grid w-full grid-rows-[min-content] gap-8 px-4 py-6 md:px-10 md:py-8"
+    >
       <h1 class="text-preset-1">
         {{ defaultLayoutAttrs.defaultLayoutLabel }}
       </h1>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,25 @@
+<script setup lang="ts">
+import capitalize from "lodash/capitalize";
+
+const financeStore = useFinanceStore();
+
+const { balance } = storeToRefs(financeStore);
+</script>
 <template>
-  <!-- <Sidebar /> -->
+  <div class="contents">
+    <!--Balance Group-->
+    <div class="grid gap-x-6 gap-y-3 md:flex">
+      <template v-for="[label, amount] of Object.entries(balance)">
+        <div
+          class="grid h-min w-full gap-3 rounded-xl p-5"
+          :class="label === 'current' ? 'bg-grey-900 text-white' : 'bg-white'"
+        >
+          <div>
+            {{ capitalize(label === "current" ? `${label} balance` : label) }}
+          </div>
+          <div class="text-preset-1">{{ formatCurrency(amount) }}</div>
+        </div>
+      </template>
+    </div>
+  </div>
 </template>
