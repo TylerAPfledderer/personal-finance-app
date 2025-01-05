d618388741d9f136065b4ccf1eb9861d54df8d8b
tylerapfledderer <tyler.pfledderer@gmail.com>
2025-01-04 21:59:09 -0500

    refactor(layouts): set dynamic heading for default layout

--- app.vue
+++ app.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
+import capitalize from "lodash/capitalize";
 const router = useRouter();
 const isAuthPage = computed(() =>
   ["/login", "/sign-up"].includes(router.currentRoute.value.fullPath),
 );
+
+const defaultLayoutLabel = computed(() => {
+  if (isAuthPage.value) return null;
+
+  const currentRoute = router.currentRoute.value.name;
+
+  return currentRoute === "index"
+    ? capitalize("overview")
+    : capitalize(currentRoute?.toString().replace("-", " "));
+});
 </script>
 <template>
-  <NuxtLayout :name="isAuthPage ? 'auth-page' : 'default'">
+  <NuxtLayout :name="isAuthPage ? 'auth-page' : 'default'" :defaultLayoutLabel>
     <NuxtPage page-key="static" />
   </NuxtLayout>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,12 @@
+<script setup lang="ts">
+const defaultLayoutAttrs = useAttrs();
+</script>
 <template>
   <Sidebar />
-  <slot></slot>
+  <main class="grid gap-8 px-4 py-6">
+    <h1 class="text-preset-1">
+      {{ defaultLayoutAttrs.defaultLayoutLabel }}
+    </h1>
+    <slot></slot>
+  </main>
 </template>
